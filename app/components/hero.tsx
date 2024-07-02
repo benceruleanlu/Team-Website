ad58add165b1d59f0152686156f95f52b21a4521
Benjamin Lu <benceruleanlu@proton.me>
2024-07-02 12:08:27 -0400

    feat: add hero

---
+++ app/components/hero.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <Spline
+      scene="https://prod.spline.design/WG9UFhrJ7C7BVk0y/scene.splinecode" 
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Hero from './components/hero';
+
 export default function Home() {
   return (
-    <p>Development is paused until basic wireframe is completed</p>
+    <div className='bg-[#050505] text-white'>
+      <Hero />
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+      <p>Placeholder for content here</p>
+    </div>
   );
 }
