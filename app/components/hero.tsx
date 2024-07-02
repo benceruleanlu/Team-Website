9c0e94eeb9404109364f82eadc2bd42d5f8713c3
Benjamin Lu <benceruleanlu@proton.me>
2024-07-02 12:28:15 -0400

    refactor: hero to inbuilt page

--- app/components/hero.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline/next';
-
-export default function Home() {
-  return (
-    <Spline
-      scene="https://prod.spline.design/WG9UFhrJ7C7BVk0y/scene.splinecode" 
-    />
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from './components/hero';
+import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
     <div className='bg-[#050505] text-white'>
-      <Hero />
+      <Spline scene="https://prod.spline.design/WG9UFhrJ7C7BVk0y/scene.splinecode"/>
       <p>Placeholder for content here</p>
       <p>Placeholder for content here</p>
       <p>Placeholder for content here</p>
