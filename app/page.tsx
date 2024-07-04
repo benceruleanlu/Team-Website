eb2ff9ef7312164412002e34beac7f1c72b4ae65
Benjamin Lu <benceruleanlu@proton.me>
2024-07-04 11:48:31 -0400

    chore: add blueprint comments of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -27,61 +27,45 @@ export default function Home() {
           <div className="text-center my-6 space-y-4 w-11/12 mx-auto">
             <p className="text-5xl font-semibold tracking-tight"><span className="bg-clip-text text-transparent bg-gradient-to-r from-white to-zinc-400">Training future engineers</span></p>
             <p className="text-xl text-slate-11">Pushing the boundaries of robotics and engineering excellence</p>
+            {/* Sponsor button */}
+            {/* Join button */}
           </div>
         </div>
 
-        {/* Content */}
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            corrupti ea hic vero praesentium nulla fugit iure neque eveniet
-            labore aliquid doloremque, eligendi mollitia ullam beatae non
-            asperiores a illo commodi ipsum nihil maiores nemo pariatur. Quia
-            vel ipsam pariatur voluptatum, autem modi, quaerat rem tempore rerum
-            voluptas, iure mollitia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            corrupti ea hic vero praesentium nulla fugit iure neque eveniet
-            labore aliquid doloremque, eligendi mollitia ullam beatae non
-            asperiores a illo commodi ipsum nihil maiores nemo pariatur. Quia
-            vel ipsam pariatur voluptatum, autem modi, quaerat rem tempore rerum
-            voluptas, iure mollitia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            corrupti ea hic vero praesentium nulla fugit iure neque eveniet
-            labore aliquid doloremque, eligendi mollitia ullam beatae non
-            asperiores a illo commodi ipsum nihil maiores nemo pariatur. Quia
-            vel ipsam pariatur voluptatum, autem modi, quaerat rem tempore rerum
-            voluptas, iure mollitia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            corrupti ea hic vero praesentium nulla fugit iure neque eveniet
-            labore aliquid doloremque, eligendi mollitia ullam beatae non
-            asperiores a illo commodi ipsum nihil maiores nemo pariatur. Quia
-            vel ipsam pariatur voluptatum, autem modi, quaerat rem tempore rerum
-            voluptas, iure mollitia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-            corrupti ea hic vero praesentium nulla fugit iure neque eveniet
-            labore aliquid doloremque, eligendi mollitia ullam beatae non
-            asperiores a illo commodi ipsum nihil maiores nemo pariatur. Quia
-            vel ipsam pariatur voluptatum, autem modi, quaerat rem tempore rerum
-            voluptas, iure mollitia.
-          </p>
-        </div>
+        {/* Sponsored By */}
+
+        {/* Feature Showcase 1: Technical design */}
+        {/* Feature Showcase 2: Controls & web programming */}
+        {/* Feature Showcase 3: Mechanical build */}
+        
+        {/* Testimonials from alums */}
+
+        {/* Last call CTA */}
+
+        {/* Footer */}
       </div>
 
       {/* Desktop site */}
       <div className="hidden bg-[#050505] text-white md:inline">
+        {/* Hero */}
         <Spline
           scene="https://prod.spline.design/WG9UFhrJ7C7BVk0y/scene.splinecode"
           className="hidden md:block"
         />
+
+        {/* Sponsored By */}
+
+        {/* Feature Showcase 1: Technical design */}
+        {/* Feature Showcase 2: Controls & web programming */}
+        {/* Feature Showcase 3: Mechanical build */}
+        
+        {/* Testimonials from alums */}
+
+        {/* Last call CTA */}
+
+        {/* Footer */}
       </div>
     </>
   );
 }
+
