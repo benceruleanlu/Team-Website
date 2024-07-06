16607b11934d9c949bea384f385f90fd5be634f4
Benjamin Lu <benceruleanlu@proton.me>
2024-07-06 12:42:49 -0400

    feat: add last call cta and footer placeholder

--- app/components/fancyDivider.tsx
+++ app/components/fancyDivider.tsx
@@ -1,6 +1,6 @@
-export default function FancyDivider() {
+export default function FancyDivider({ curve, sharp }: {curve?: boolean, sharp?: boolean}) {
   return (
-    <div className="border-t rounded-3xl w-screen h-24mx-auto px-6 py-5 sm:py-24 max-w-5xl md:max-w-7xl relative border-t border-[#d6ebfd30] mt-24 flex flex-col items-center">
+    <div className={`border-t ${curve ? 'rounded-3xl' : ''} w-screen h-24mx-auto px-6 py-5 sm:py-24 max-w-5xl md:max-w-7xl relative border-t border-[#d6ebfd30] mt-16 flex flex-col items-center`}>
       <div
         className="left-1/2 top-0 w-[300px] max-w-[300px] pointer-events-none absolute h-px max-w-full -translate-x-1/2 -translate-y-1/2"
         style={{
@@ -8,7 +8,7 @@ export default function FancyDivider() {
         }}
       ></div>
       <div
-        className="-top-1 left-1/2 h-[200px] w-full max-w-[200px] md:max-w-[400px] pointer-events-none absolute max-w-full -translate-x-1/2 -translate-y-1/2"
+        className={`-top-1 left-1/2 ${sharp ? 'h-[100px]' : 'h-[200px]'} w-full max-w-[200px] md:max-w-[400px] pointer-events-none absolute max-w-full -translate-x-1/2 -translate-y-1/2`}
         style={{
           background: "conic-gradient(from 90deg at 50% 50%, rgba(0, 0, 0, 0) 50%, rgb(5, 5, 5) 50%), radial-gradient(rgba(200, 200, 200, 0.1) 0%, transparent 80%)"
         }}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar";
 import SponsorCarousel from "./components/sponsorCarousel";
 import Button from "./components/button";
 import FancyDivider from "./components/fancyDivider";
-import Feature from "./components/feature";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,9 +16,9 @@ export default function Home() {
             scene="https://prod.spline.design/5j0HM0tVtyZUlBjh/scene.splinecode"
             style={{ width: "100vw", height: "40vh" }}
           />
-          <div className="mx-auto w-11/12 space-y-6 text-center">
+          <div className="mx-auto w-11/12 space-y-6 text-center mb-24">
             <p className="text-5xl font-semibold leading-tight tracking-tight">
-              <span className="bg-gradient-to-r from-white to-zinc-400 bg-clip-text text-transparent">
+              <span className="bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-transparent">
                 Training future engineers
               </span>
             </p>
@@ -40,23 +40,62 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <FancyDivider />
+        <FancyDivider curve={true} sharp={false}/>
         <SponsorCarousel />
 
         {/* Feature Showcase 1: Technical design */}
-        <Feature
-          spline="https://prod.spline.design/H3HOtgb0Lpics6r2/scene.splinecode"
-          title="Technical Design"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ducimus minus ipsa reprehenderit veritatis doloribus molestiae."
-        />
+        <div className="mt-28">
+          <div className="mx-auto w-11/12 space-y-6 text-center">
+            <p className="text-5xl leading-tight tracking-tight">
+              Lorem
+              <span> </span>
+              <span className="bg-gradient-to-br from-[#E131F3] via-[#59B2EA] to-[#A7FC8F] bg-clip-text text-transparent">
+                ipsum
+              </span>
+            </p>
+            <p className="px-1 text-neutral-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deserunt id reprehenderit corrupti minus necessitatibus.
+            </p>
+            <Image
+              src="/robot.png"
+              alt="lorem ipsum"
+              height={300}
+              width={300}
+              className="object-fit w-screen"
+            />
+          </div>
+        </div>
         {/* Feature Showcase 2: Controls & web programming */}
         {/* Feature Showcase 3: Mechanical build */}
 
         {/* Testimonials from alums */}
 
         {/* Last call CTA */}
+        <div className="mx-auto w-11/12 space-y-6 text-center mt-24">
+          <p className="text-5xl font-semibold leading-tight tracking-tight">
+            <span className="bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-transparent">
+              Get involved.
+            </span>
+          </p>
+          <div className="flex flex-col space-y-2">
+            <Button
+              text="Sponsor"
+              link="/sponsor"
+              className="flex flex-row justify-center rounded-full bg-white p-3 font-semibold text-black"
+            />
+            <Button
+              text="Join"
+              link="/join"
+              className="flex flex-row justify-center rounded-full p-3 font-semibold text-neutral-400"
+            />
+          </div>
+        </div>
 
         {/* Footer */}
+        <FancyDivider curve={false} sharp={true}/>
+        <div>
+          <p>Placeholder for footer</p>
+        </div>
 
         <div className="h-screen w-screen bg-[#050505]"></div>
       </div>
