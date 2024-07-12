08878e1d9ed4b0fb8067bde3c196c73c12720861
Benjamin Lu <benceruleanlu@proton.me>
2024-07-12 17:02:52 -0400

    feat: add sponsor page brick

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <>
+      <p>Placeholder for /Contact page.</p>
+    </>
+  );
+}
--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -1,10 +1,39 @@
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Sponsors from "../components/sponsorCarousel";
+import Link from "next/link";
 
 export default function Sponsor() {
   return (
     <>
       <Navbar />
-      <p>Placeholder for /sponsor page</p>
+      <div className="mx-auto mt-20 mb-12 w-11/12 space-y-2 text-center">
+        <p className="text-5xl leading-tight tracking-tight">
+          <span className="bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-transparent">
+            Lorem
+          </span>
+        </p>
+        <p className="px-1 text-neutral-400">
+          Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="w-11/12 rounded-3xl mx-auto border border-[#d6ebfd30] mb-8 text-center">
+        <p className="mt-6 mb-8">Mini</p>
+        <p className="text-4xl mb-8">$250+</p>
+        <ul className="text-[#f1f7feb5] text-sm w-10/12 mx-auto">
+          <li className="flex flex-row text-left space-x-2">
+            <svg className="text-[#43fea4ab]" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM15.5312 10.5312L11.5312 14.5312C11.2375 14.825 10.7625 14.825 10.4719 14.5312L8.47188 12.5312C8.17813 12.2375 8.17813 11.7625 8.47188 11.4719C8.76563 11.1813 9.24062 11.1781 9.53125 11.4719L11 12.9406L14.4688 9.46875C14.7625 9.175 15.2375 9.175 15.5281 9.46875C15.8187 9.7625 15.8219 10.2375 15.5281 10.5281L15.5312 10.5312Z" fill="currentColor"></path></svg>
+            <p className="pt-[0.11em]">Name & logo on shirts and social media</p>
+          </li>
+        </ul>
+        <Link href="/contact">
+          <div className="bg-white text-black mt-4 mb-6 w-10/12 rounded-full mx-auto py-2 text-sm font-semibold">
+            Contact Us
+          </div>
+        </Link>
+      </div>
+      <Sponsors/>
+      <Footer />
     </>
   );
 }
