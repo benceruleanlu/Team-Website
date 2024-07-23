4cb778c65ab8b6ee4beb4f405d6e539ff670ea3b
Benjamin Lu <benceruleanlu@proton.me>
2024-07-22 20:46:01 -0400

    feat: add title to join page

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -4,7 +4,17 @@ export default function Sponsor() {
   return (
     <>
       <Navbar />
-      <p>Placeholder for /join page</p>
+      <div className="mx-auto my-20 w-11/12 text-center">
+        <p className="text-6xl font-semibold leading-tight tracking-tight">
+          <span className="bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-transparent">
+            Join Us
+          </span>
+        </p>
+        <p className="px-1 text-neutral-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+          ducimus minus ipsa reprehenderit veritatis doloribus molestiae.
+        </p>
+      </div>
     </>
   );
 }
