98b33466768bb84cfb6555965b3e2fca02b4e901
Benjamin Lu <benceruleanlu@proton.me>
2024-07-31 23:04:04 -0400

    feat: add hero text MAY BE CHANGED

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,14 @@ export default function Home() {
             className={"hidden lg:block"}
           />
           <div className="mx-auto mb-24 w-11/12 space-y-6 text-center md:mt-24 md:text-left">
-            <p className="text-[2.5rem] font-semibold leading-tight tracking-tight md:text-6xl">
+            <p className="text-[3.25rem] font-semibold leading-tight tracking-tight subpixel-antialiased md:text-6xl">
               <span className="bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-transparent">
                 Training future engineers
               </span>
             </p>
             <p className="px-1 text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-              ducimus minus ipsa reprehenderit veritatis doloribus molestiae.
+              An opportunity for students to explore robotics. Get trained in
+              design, precision machining, and programming.
             </p>
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-4 md:space-y-0">
               <Button
