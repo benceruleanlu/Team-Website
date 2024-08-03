0cf9d6b1afa5a8f4e2a3e8be2c5db35a20885ac0
Benjamin Lu <benceruleanlu@proton.me>
2024-08-03 15:03:49 -0400

    feat: blog waiting page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,12 @@ export default function Sponsor() {
   return (
     <>
       <Navbar />
-      <p>Placeholder for /blog page</p>
+      <div className="font-base flex min-h-screen items-center justify-center text-center text-white">
+        <div>
+          <p>Our team is working hard to bring you the latest updates!</p>
+          <p>Stay tuned for new blog posts coming soon.</p>
+        </div>
+      </div>
     </>
   );
 }
