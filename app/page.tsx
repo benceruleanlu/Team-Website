c6efb664aebea8a5043b29266c07c25af5a1349c
Benjamin Lu <benceruleanlu@proton.me>
2024-08-03 14:38:29 -0400

    chore: remove useless imports due to missing blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Spline from "@splinetool/react-spline/next";
-import Image from "next/image";
+// import Image from "next/image";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import SponsorCarousel from "./components/sponsorCarousel";
 import Button from "./components/button";
 import FancyDivider from "./components/fancyDivider";
 import CodeEmbed from "./components/codeEmbed";
-import Link from "next/link";
+// import Link from "next/link";
 import Carousel from "./components/carousel";
 
 export default function Home() {
