16190064b70fc08b70fb594f5362cee0670dc0b8
Benjamin Lu <benceruleanlu@proton.me>
2024-08-25 20:26:22 -0400

    Set X-Content-Type-Options to nosniff

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,10 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: cspHeader.replace(/\n/g, ''),
           },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          }
         ],
       },
     ];
